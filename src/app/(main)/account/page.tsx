ad4a8b93cc956bcab349cbb4712c2a692bff979f
advplyr <advplyr@protonmail.com>
2025-08-11 17:43:00 -0500

    Add i18n language options, update account page, add color-border

--- src/app/(main)/account/page.tsx
+++ src/app/(main)/account/page.tsx
@@ -1,4 +1,9 @@
-import { getCurrentUser } from '../../../lib/api'
+import TextInput from '@/components/ui/TextInput'
+import Dropdown from '@/components/ui/Dropdown'
+
+import { getCurrentUser } from '@/lib/api'
+import { getLanguageCodeOptions } from '@/lib/i18n'
+
 import LogoutBtn from './LogoutBtn'
 
 export const dynamic = 'force-dynamic'
@@ -7,12 +12,29 @@ export default async function AccountPage() {
   const userResponse = await getCurrentUser()
   const user = userResponse.data?.user
 
+  const languageOptions = getLanguageCodeOptions()
+
+  if (!user) {
+    return null
+  }
+
   return (
     <div className="p-8 w-full max-w-xl mx-auto">
       <h1 className="text-2xl">Account</h1>
 
       <div className="flex flex-col items-start gap-4 mt-8">
-        <p>Username: {user?.username}</p>
+        <div className="flex items-center gap-2 w-full">
+          <div className="flex-2">
+            <TextInput value={user.username} label="Username" readOnly />
+          </div>
+          <div className="flex-1">
+            <TextInput value={user.type} label="Account Type" readOnly />
+          </div>
+        </div>
+        <div className="w-full">
+          <Dropdown value={'en-us'} items={languageOptions} label="Language" />
+        </div>
+        <div className="w-full h-px bg-border" />
         <LogoutBtn />
       </div>
     </div>
