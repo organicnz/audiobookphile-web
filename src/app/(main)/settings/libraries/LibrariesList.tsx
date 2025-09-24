be8b455d38b36f3cae90b4e95cb984cd44b3ac7b
advplyr <advplyr@protonmail.com>
2025-09-24 17:18:58 -0500

    Update libraries table styles, link to library, update drag handle, add library icon

--- src/app/(main)/settings/libraries/LibrariesList.tsx
+++ src/app/(main)/settings/libraries/LibrariesList.tsx
@@ -1,7 +1,10 @@
 'use client'
 
 import { useState } from 'react'
+import Link from 'next/link'
+import { mergeClasses } from '@/lib/merge-classes'
 import SortableList from '@/components/widgets/SortableList'
+import LibraryIcon from '@/components/ui/LibraryIcon'
 
 export default function LibrariesList(props: { libraries: any[] }) {
   const { libraries: librariesData } = props
@@ -19,9 +22,20 @@ export default function LibrariesList(props: { libraries: any[] }) {
         }}
         renderItem={(item: any, index: number) => {
           return (
-            <div className="flex items-center gap-2 p-4">
-              <div className="drag-handle">⋮⋮</div>
-              {item && <div>{item.name as string}</div>}
+            <div
+              className={mergeClasses(
+                'flex items-center gap-4 py-2 px-4 hover:bg-primary/20 text-white/50 hover:text-white border border-border',
+                index === 0 ? 'rounded-t-md' : index === libraries.length - 1 ? 'rounded-b-md' : ''
+              )}
+            >
+              <LibraryIcon icon={item.icon} />
+              <Link className="text-white hover:underline" href={`/library/${item.id}`}>
+                {item.name as string}
+              </Link>
+              <div className="grow" />
+              <div className="drag-handle">
+                <span className="material-symbols text-xl text-white/50 hover:text-white">reorder</span>
+              </div>
             </div>
           )
         }}
