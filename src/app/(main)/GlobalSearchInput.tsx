da609e6573ef5f2f59b62da2191c22ba081804f3
mikiher <mikiher@gmail.com>
2025-10-07 16:56:58 +0300

    GlobalSearchInput: Use size "small", fix RTL

--- src/app/(main)/GlobalSearchInput.tsx
+++ src/app/(main)/GlobalSearchInput.tsx
@@ -6,9 +6,9 @@ import { useState } from 'react'
 export default function GlobalSearchInput() {
   const [searchValue, setSearchValue] = useState('')
   return (
-    <div className="w-80 text-sm relative hidden md:block">
-      <InputDropdown items={[]} placeholder="Search..." value={searchValue} onChange={(value) => setSearchValue(String(value))} />
-      <div className="absolute right-0 top-0 h-full w-10 flex items-center justify-center">
+    <div className="relative hidden md:block">
+      <InputDropdown items={[]} size="small" placeholder="Search..." value={searchValue} onChange={(value) => setSearchValue(String(value))} />
+      <div className="absolute end-0 top-0 h-full w-10 flex items-center justify-center">
         <span className="material-symbols text-gray-400 text-lg">search</span>
       </div>
     </div>
