913835969e6e7a6b7cc23c0bceb416b5d1d15173
advplyr <advplyr@protonmail.com>
2025-10-24 17:10:32 -0500

    Fix tags/genres page to use getData api wrapper

--- src/app/(main)/settings/item-metadata-utils/genres/page.tsx
+++ src/app/(main)/settings/item-metadata-utils/genres/page.tsx
@@ -1,4 +1,4 @@
-import { getGenres } from '@/lib/api'
+import { getData, getGenres } from '@/lib/api'
 import { getTypeSafeTranslations } from '@/lib/getTypeSafeTranslations'
 import SettingsContent from '../../SettingsContent'
 import GenresClient from './GenresClient'
@@ -8,8 +8,8 @@ export const dynamic = 'force-dynamic'
 export default async function ItemMetadataUtilsGenresPage() {
   const t = await getTypeSafeTranslations()
 
-  const genresResponse = await getGenres()
-  const genres = genresResponse.data?.genres || []
+  const [genresResponse] = await getData(getGenres())
+  const genres = genresResponse?.genres || []
 
   return (
     <SettingsContent title={t('HeaderManageGenres')} backLink="/settings/item-metadata-utils">
--- src/app/(main)/settings/item-metadata-utils/tags/page.tsx
+++ src/app/(main)/settings/item-metadata-utils/tags/page.tsx
@@ -1,4 +1,4 @@
-import { getTags } from '@/lib/api'
+import { getData, getTags } from '@/lib/api'
 import { getTypeSafeTranslations } from '@/lib/getTypeSafeTranslations'
 import SettingsContent from '../../SettingsContent'
 import TagsClient from './TagsClient'
@@ -8,8 +8,8 @@ export const dynamic = 'force-dynamic'
 export default async function ItemMetadataUtilsTagsPage() {
   const t = await getTypeSafeTranslations()
 
-  const tagsResponse = await getTags()
-  const tags = tagsResponse.data?.tags || []
+  const [tagsResponse] = await getData(getTags())
+  const tags = tagsResponse?.tags || []
 
   return (
     <SettingsContent title={t('HeaderManageTags')} backLink="/settings/item-metadata-utils">
