9a8ac1f2c948a2b8557e53384d8ab9e735e5822e
mikiher <mikiher@gmail.com>
2025-10-26 08:25:36 +0200

    LibraryClient: Enhance type safety by defining explicit prop types

--- src/app/(main)/library/[library]/LibraryClient.tsx
+++ src/app/(main)/library/[library]/LibraryClient.tsx
@@ -1,8 +1,10 @@
 'use client'
 
+import { Library, PersonalizedShelf } from '@/types/api'
+
 interface LibraryClientProps {
-  library: any
-  personalized: any
+  library: Library
+  personalized: PersonalizedShelf[]
 }
 
 export default function LibraryClient({ library, personalized }: LibraryClientProps) {
