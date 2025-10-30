4038ada83687d579dc4d1ccd67ae29597a5f74ef
advplyr <advplyr@protonmail.com>
2025-10-30 17:43:09 -0500

    Update settings page wrapper theme colors, add foreground-muted color

--- src/app/(main)/settings/SettingsMoreInfoIcon.tsx
+++ src/app/(main)/settings/SettingsMoreInfoIcon.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 export default function SettingsMoreInfoIcon(props: { moreInfoUrl: string }) {
   return (
     <Tooltip text="Click for more info" position="right" closeOnClick className="leading-0">
-      <Link href={props.moreInfoUrl} target="_blank" className="leading-0 text-gray-200">
+      <Link href={props.moreInfoUrl} target="_blank" className="leading-0 text-foreground-muted">
         <span className="material-symbols text-lg">help_outline</span>
       </Link>
     </Tooltip>
