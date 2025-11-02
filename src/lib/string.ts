1c0abc734ed51176aff8c90dede410ae4c36d3f4
mikiher <mikiher@gmail.com>
2025-11-02 11:19:55 +0200

    Add bytesPretty function to format byte sizes in a human-readable way

--- src/lib/string.ts
+++ src/lib/string.ts
@@ -1,3 +1,14 @@
 export const capitalizeFirstLetter = (string: string): string => {
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
+
+export const bytesPretty = (bytes: number, decimals: number = 2): string => {
+  if (isNaN(bytes) || bytes == 0) {
+    return '0 Bytes'
+  }
+  const k = 1000
+  const dm = decimals < 0 ? 0 : decimals
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+  const i = Math.floor(Math.log(bytes) / Math.log(k))
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
+}
