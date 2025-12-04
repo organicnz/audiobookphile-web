69a62964bb8395735b9de1f433e47b2bc61de48b
advplyr <advplyr@protonmail.com>
2025-12-04 17:12:44 -0600

    Update series page to use series cards & cover size widget

--- src/app/(main)/library/[library]/(book)/series/SeriesClient.tsx
+++ src/app/(main)/library/[library]/(book)/series/SeriesClient.tsx
@@ -1,17 +1,39 @@
 'use client'
 
-import { GetSeriesResponse } from '@/types/api'
+import CoverSizeWidget from '@/components/widgets/CoverSizeWidget'
+import { SeriesCard } from '@/components/widgets/media-card/SeriesCard'
+import { BookshelfView, GetSeriesResponse, Library, UserLoginResponse } from '@/types/api'
 
 interface SeriesClientProps {
   seriesData: GetSeriesResponse
+  library: Library
+  currentUser: UserLoginResponse
 }
 
-export default function SeriesClient({ seriesData }: SeriesClientProps) {
+export default function SeriesClient({ seriesData, library, currentUser }: SeriesClientProps) {
+  const seriesResults = seriesData.results
+  const dateFormat = currentUser?.serverSettings?.dateFormat ?? 'MM/dd/yyyy'
+  const libraryId = library.id
+
+  console.log('currentUser', currentUser)
+
   return (
     <div>
-      <div className="mb-6 p-4 bg-primary rounded-lg">
-        <pre className="text-sm bg-black p-2 rounded border whitespace-pre-wrap overflow-x-auto">{JSON.stringify(seriesData, null, 2)}</pre>
+      <div className="flex justify-center flex-wrap gap-4">
+        {seriesResults.map((series) => {
+          return (
+            <SeriesCard
+              key={series.id}
+              series={series}
+              libraryId={libraryId}
+              bookshelfView={BookshelfView.DETAIL}
+              dateFormat={dateFormat}
+              bookCoverAspectRatio={library.settings?.coverAspectRatio ?? 1}
+            />
+          )
+        })}
       </div>
+      <CoverSizeWidget className="absolute bottom-4 right-4 z-10" />
     </div>
   )
 }
