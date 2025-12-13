443825d616e3ee4a4ce093aae076530059b45ace
advplyr <advplyr@protonmail.com>
2025-12-13 16:15:18 -0600

    Add AuthorCards to authors page

--- src/app/(main)/library/[library]/(book)/authors/AuthorsClient.tsx
+++ src/app/(main)/library/[library]/(book)/authors/AuthorsClient.tsx
@@ -1,36 +1,22 @@
 'use client'
 
-import { useTypeSafeTranslations } from '@/hooks/useTypeSafeTranslations'
-import { GetAuthorsResponse } from '@/types/api'
-import Image from 'next/image'
-import Link from 'next/link'
+import { AuthorCard } from '@/components/widgets/media-card/AuthorCard'
+import { GetAuthorsResponse, UserLoginResponse } from '@/types/api'
 
 interface AuthorsClientProps {
   authorsData: GetAuthorsResponse
+  currentUser: UserLoginResponse
 }
 
-export default function AuthorsClient({ authorsData }: AuthorsClientProps) {
-  const t = useTypeSafeTranslations()
+export default function AuthorsClient({ authorsData, currentUser }: AuthorsClientProps) {
   const authors = authorsData.authors
+  const user = currentUser.user
 
   return (
     <div>
       <div className="flex flex-wrap gap-2">
         {authors.map((author) => {
-          const imageUrl = author.imagePath ? `/api/authors/${author.id}/image` : null
-          return (
-            <Link
-              key={author.id}
-              href={`/author/${author.id}`}
-              className="p-4 rounded-lg border border-border bg-primary/30 hover:bg-primary/70 cursor-pointer w-40 h-48 relative flex items-center justify-center text-center overflow-hidden"
-            >
-              {imageUrl && <Image src={imageUrl} unoptimized alt={author.name} fill objectFit="cover" />}
-              <div className="absolute left-0 right-0 bottom-0 p-2 bg-black/50 text-white">
-                <h3 className="text-xs font-semibold mb-1 truncate">{author.name}</h3>
-                <p className="text-xs text-gray-300">{t('LabelXBooks', { count: author.numBooks ?? 0 })}</p>
-              </div>
-            </Link>
-          )
+          return <AuthorCard key={author.id} author={author} userCanUpdate={user.permissions.update} />
         })}
       </div>
     </div>
--- src/app/(main)/library/[library]/(book)/authors/page.tsx
+++ src/app/(main)/library/[library]/(book)/authors/page.tsx
@@ -1,13 +1,13 @@
-import { getAuthors, getData } from '@/lib/api'
+import { getAuthors, getCurrentUser, getData } from '@/lib/api'
 import AuthorsClient from './AuthorsClient'
 
 export default async function AuthorsPage({ params }: { params: Promise<{ library: string }> }) {
   const { library: libraryId } = await params
-  const [authorsData] = await getData(getAuthors(libraryId))
+  const [authorsData, currentUser] = await getData(getAuthors(libraryId), getCurrentUser())
 
   return (
     <div className="p-8 w-full">
-      <AuthorsClient authorsData={authorsData} />
+      <AuthorsClient authorsData={authorsData} currentUser={currentUser} />
     </div>
   )
 }
