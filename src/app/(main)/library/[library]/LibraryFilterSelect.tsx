8fb70ec0e918ec19c793e7786cb0ad0ff5e8e46a
mikiher <mikiher@gmail.com>
2025-12-26 07:48:52 +0200

    LibraryFilterSelect: Revert menuMaxHeight to 80vh

--- src/app/(main)/library/[library]/LibraryFilterSelect.tsx
+++ src/app/(main)/library/[library]/LibraryFilterSelect.tsx
@@ -357,7 +357,7 @@ export default function LibraryFilterSelect({ isSeries = false, user }: LibraryF
         size="auto"
         className="h-full text-xs"
         displayText={getSelectedText()}
-        menuMaxHeight="200px"
+        menuMaxHeight="80vh"
       />
       {showClear && (
         <button
