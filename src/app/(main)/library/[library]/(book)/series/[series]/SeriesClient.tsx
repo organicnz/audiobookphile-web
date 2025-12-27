56d3a3f4fd612983f43d5000f5246ca24e8cd978
advplyr <advplyr@protonmail.com>
2025-12-27 16:30:35 -0600

    Add series page back

---
+++ src/app/(main)/library/[library]/(book)/series/[series]/SeriesClient.tsx
@@ -0,0 +1,35 @@
+import BookMediaCard from '@/components/widgets/media-card/BookMediaCard'
+import { BookshelfView, GetLibraryItemsResponse, Library, UserLoginResponse } from '@/types/api'
+
+interface SeriesClientProps {
+  currentUser: UserLoginResponse
+  library: Library
+  libraryItems: GetLibraryItemsResponse
+}
+
+export default function SeriesClient({ currentUser, library, libraryItems }: SeriesClientProps) {
+  const userMediaProgress = currentUser.user.mediaProgress
+  return (
+    <div>
+      <div className="flex flex-wrap gap-4">
+        {libraryItems.results.map((libraryItem) => {
+          const entityProgress = userMediaProgress.find((progress) => progress.libraryItemId === libraryItem.id)
+          return (
+            <BookMediaCard
+              key={libraryItem.id}
+              libraryItem={libraryItem}
+              bookshelfView={BookshelfView.DETAIL}
+              dateFormat={currentUser.serverSettings?.dateFormat ?? 'MM/dd/yyyy'}
+              timeFormat={currentUser.serverSettings?.timeFormat ?? 'HH:mm'}
+              userPermissions={currentUser.user.permissions}
+              ereaderDevices={currentUser.ereaderDevices}
+              showSubtitles={true}
+              bookCoverAspectRatio={library.settings?.coverAspectRatio ?? 1}
+              mediaProgress={entityProgress}
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
---
+++ src/app/(main)/library/[library]/(book)/series/[series]/page.tsx
@@ -0,0 +1,23 @@
+import { getCurrentUser, getData, getLibrary, getLibraryItems, getSeries } from '@/lib/api'
+import SeriesClient from './SeriesClient'
+
+export default async function SeriesPage({ params }: { params: Promise<{ series: string; library: string }> }) {
+  const { series: seriesId, library: libraryId } = await params
+  const [series, currentUser, library, libraryItems] = await getData(
+    getSeries(libraryId, seriesId),
+    getCurrentUser(),
+    getLibrary(libraryId),
+    getLibraryItems(libraryId, `filter=series.${encodeURIComponent(Buffer.from(seriesId).toString('base64'))}`)
+  )
+
+  if (!series || !currentUser || !library || !libraryItems) {
+    console.error('Error getting series or user or library or library items data')
+    return null
+  }
+
+  return (
+    <div className="p-8 w-full">
+      <SeriesClient currentUser={currentUser} library={library} libraryItems={libraryItems} />
+    </div>
+  )
+}
