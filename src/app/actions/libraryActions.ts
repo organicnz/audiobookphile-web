a9a706b70b6e641545bcbad0730fe44dd1a7a6b8
mikiher <mikiher@gmail.com>
2025-12-27 09:05:57 +0200

    libraryActions: Add server actions for authors, collections, series, and playlists

--- src/app/actions/libraryActions.ts
+++ src/app/actions/libraryActions.ts
@@ -1,6 +1,6 @@
 'use server'
 
-import { getLibraryFilterData, getLibraryItems } from '@/lib/api'
+import { getLibraryAuthors, getLibraryCollections, getLibraryFilterData, getLibraryItems, getLibraryPlaylists, getLibrarySeries } from '@/lib/api'
 
 export async function fetchLibraryItemsAction(libraryId: string, query: string) {
   return getLibraryItems(libraryId, query)
@@ -8,4 +8,20 @@ export async function fetchLibraryItemsAction(libraryId: string, query: string) 
 
 export async function fetchLibraryFilterDataAction(libraryId: string) {
   return getLibraryFilterData(libraryId)
+}
+
+export async function fetchSeriesAction(libraryId: string, query: string) {
+  return getLibrarySeries(libraryId, query)
+}
+
+export async function fetchAuthorsAction(libraryId: string, query: string) {
+  return getLibraryAuthors(libraryId, query)
+}
+
+export async function fetchCollectionsAction(libraryId: string, query: string) {
+  return getLibraryCollections(libraryId, query)
+}
+
+export async function fetchPlaylistsAction(libraryId: string, query: string) {
+  return getLibraryPlaylists(libraryId, query)
 }
