cf375b4dc8bf1ef38b40f31bbc69b19338fbe05d
Dan Dragon <danielcanedragon@gmail.com>
2026-01-04 19:24:36 +0000

    Ability to add, or not add, the tabstop for the tooltip.

--- src/app/(main)/settings/SettingsToggleSwitch.tsx
+++ src/app/(main)/settings/SettingsToggleSwitch.tsx
@@ -14,8 +14,10 @@ export default function SettingsToggleSwitch(props: SettingsToggleSwitchProps) {
     <div className="flex items-center">
       <ToggleSwitch label={props.label} value={props.value} onChange={props.onChange} disabled={props.disabled ?? false} />
       {props.tooltip && (
-        <Tooltip text={props.tooltip} position="right" maxWidth={300} className="-ml-2">
-          <span className="material-symbols text-lg" aria-hidden="true">info</span>
+        <Tooltip text={props.tooltip} position="right" maxWidth={300} className="-ml-2" addTabIndex={true}>
+          <span className="material-symbols text-lg" aria-hidden="true">
+            info
+          </span>
         </Tooltip>
       )}
     </div>
