9329f55ea40b219cc50e760a2675ed976b9ac8a4
advplyr <advplyr@protonmail.com>
2026-01-12 17:25:51 -0600

    Add automatic backups setting and cron expression preview

--- src/app/(main)/settings/backups/BackupsClient.tsx
+++ src/app/(main)/settings/backups/BackupsClient.tsx
@@ -1,17 +1,22 @@
 'use client'
 
+import CronExpressionPreview from '@/components/widgets/CronExpressionPreview'
 import { useTypeSafeTranslations } from '@/hooks/useTypeSafeTranslations'
-import { GetBackupsResponse } from '@/types/api'
+import { GetBackupsResponse, UserLoginResponse } from '@/types/api'
 import SettingsContent from '../SettingsContent'
+import SettingsToggleSwitch from '../SettingsToggleSwitch'
 import BackupLocation from './BackupLocation'
 
 interface BackupsClientProps {
   backupResponse: GetBackupsResponse
+  currentUser: UserLoginResponse
 }
 
-export default function BackupsClient({ backupResponse }: BackupsClientProps) {
+export default function BackupsClient({ backupResponse, currentUser }: BackupsClientProps) {
   const t = useTypeSafeTranslations()
   const backups = backupResponse.backups
+  const serverSettings = currentUser.serverSettings
+  const autoBackupsEnabled = !!serverSettings.backupSchedule
 
   return (
     <SettingsContent
@@ -24,6 +29,22 @@ export default function BackupsClient({ backupResponse }: BackupsClientProps) {
       <div>
         {/* backup location */}
         <BackupLocation backupLocation={backupResponse.backupLocation} />
+
+        <div className="mb-4 -ml-2">
+          <SettingsToggleSwitch
+            label={t('LabelBackupsEnableAutomaticBackups')}
+            value={autoBackupsEnabled}
+            onChange={() => {
+              console.log('Not implemented yet')
+            }}
+            tooltip={t('LabelBackupsEnableAutomaticBackupsHelp')}
+          />
+          {autoBackupsEnabled && (
+            <div className="pl-6">
+              <CronExpressionPreview cronExpression={serverSettings.backupSchedule as string} />
+            </div>
+          )}
+        </div>
 
         {/* backups */}
         {backups.map((backup) => (
--- src/app/(main)/settings/backups/page.tsx
+++ src/app/(main)/settings/backups/page.tsx
@@ -1,14 +1,14 @@
-import { getBackups, getData } from '@/lib/api'
+import { getBackups, getCurrentUser, getData } from '@/lib/api'
 import BackupsClient from './BackupsClient'
 
 export const dynamic = 'force-dynamic'
 
 export default async function BackupsPage() {
-  const [backupsResponse] = await getData(getBackups())
+  const [backupsResponse, currentUser] = await getData(getBackups(), getCurrentUser())
 
   if (!backupsResponse) {
     return <div>Error loading backups</div>
   }
 
-  return <BackupsClient backupResponse={backupsResponse} />
+  return <BackupsClient backupResponse={backupsResponse} currentUser={currentUser} />
 }
