55748035464bdae8e687148fb9e543eb770587d2
advplyr <advplyr@protonmail.com>
2026-02-20 15:33:43 -0600

    Fix MultiSelect min height

--- src/components/ui/MultiSelect.tsx
+++ src/components/ui/MultiSelect.tsx
@@ -574,7 +574,7 @@ export function MultiSelect<T = string>({
         </Label>
       )}
       <div ref={wrapperRef} className="relative">
-        <InputWrapper disabled={disabled} inputRef={inputRef} size="medium">
+        <InputWrapper disabled={disabled} inputRef={inputRef} size="auto" className="min-h-10">
           <div
             ref={inputWrapperRef}
             role="list"
