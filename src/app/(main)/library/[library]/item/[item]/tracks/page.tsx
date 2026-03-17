ec4c0330a8c506e208a8230680d1212fa5796e4a
advplyr <advplyr@protonmail.com>
2026-03-17 16:38:55 -0500

    Create stub page for library item tracks

---
+++ src/app/(main)/library/[library]/item/[item]/tracks/page.tsx
@@ -0,0 +1,22 @@
+import { getCurrentUser, getData, getLibraryItem } from '@/lib/api'
+
+export default async function TracksPage({ params }: { params: Promise<{ item: string; library: string }> }) {
+  const { item: itemId } = await params
+  const [libraryItem, currentUser] = await getData(getLibraryItem(itemId), getCurrentUser())
+
+  if (!libraryItem || !currentUser) {
+    console.error('Error getting library item or user data')
+    return null
+  }
+
+  return (
+    <div className="p-8 w-full flex flex-col gap-4">
+      <div className="bg-black p-2 rounded border">
+        <pre className="text-sm whitespace-pre-wrap overflow-x-auto">{JSON.stringify(libraryItem, null, 2)}</pre>
+      </div>
+      <div className="bg-black p-2 rounded border">
+        <pre className="text-sm whitespace-pre-wrap overflow-x-auto">{JSON.stringify(currentUser, null, 2)}</pre>
+      </div>
+    </div>
+  )
+}
