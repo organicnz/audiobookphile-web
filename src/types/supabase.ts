371220572052c970debb1c3b2c701a0da782600c
organicnz <tamerlanium@gmail.com>
2026-05-06 00:20:29 -0700

    chore: sync supabase types from successful data migration

--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -14,6 +14,365 @@ export type Database = {
   }
   public: {
     Tables: {
+      authors: {
+        Row: {
+          asin: string | null
+          created_at: string
+          description: string | null
+          id: string
+          image_path: string | null
+          last_first: string | null
+          library_id: string | null
+          name: string | null
+          updated_at: string
+        }
+        Insert: {
+          asin?: string | null
+          created_at?: string
+          description?: string | null
+          id: string
+          image_path?: string | null
+          last_first?: string | null
+          library_id?: string | null
+          name?: string | null
+          updated_at?: string
+        }
+        Update: {
+          asin?: string | null
+          created_at?: string
+          description?: string | null
+          id?: string
+          image_path?: string | null
+          last_first?: string | null
+          library_id?: string | null
+          name?: string | null
+          updated_at?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "authors_library_id_fkey"
+            columns: ["library_id"]
+            isOneToOne: false
+            referencedRelation: "libraries"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      book_authors: {
+        Row: {
+          author_id: string
+          book_id: string
+          created_at: string | null
+          id: string
+        }
+        Insert: {
+          author_id: string
+          book_id: string
+          created_at?: string | null
+          id?: string
+        }
+        Update: {
+          author_id?: string
+          book_id?: string
+          created_at?: string | null
+          id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "book_authors_author_id_fkey"
+            columns: ["author_id"]
+            isOneToOne: false
+            referencedRelation: "authors"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "book_authors_book_id_fkey"
+            columns: ["book_id"]
+            isOneToOne: false
+            referencedRelation: "books"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      book_series: {
+        Row: {
+          book_id: string
+          created_at: string | null
+          id: string
+          sequence: string | null
+          series_id: string
+        }
+        Insert: {
+          book_id: string
+          created_at?: string | null
+          id?: string
+          sequence?: string | null
+          series_id: string
+        }
+        Update: {
+          book_id?: string
+          created_at?: string | null
+          id?: string
+          sequence?: string | null
+          series_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "book_series_book_id_fkey"
+            columns: ["book_id"]
+            isOneToOne: false
+            referencedRelation: "books"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "book_series_series_id_fkey"
+            columns: ["series_id"]
+            isOneToOne: false
+            referencedRelation: "series"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      books: {
+        Row: {
+          abridged: boolean | null
+          asin: string | null
+          audio_files: Json | null
+          chapters: Json | null
+          cover_path: string | null
+          created_at: string
+          description: string | null
+          duration: number | null
+          ebook_file: Json | null
+          explicit: boolean | null
+          genres: Json | null
+          id: string
+          isbn: string | null
+          language: string | null
+          narrators: Json | null
+          published_date: string | null
+          published_year: string | null
+          publisher: string | null
+          subtitle: string | null
+          tags: Json | null
+          title: string | null
+          updated_at: string
+        }
+        Insert: {
+          abridged?: boolean | null
+          asin?: string | null
+          audio_files?: Json | null
+          chapters?: Json | null
+          cover_path?: string | null
+          created_at?: string
+          description?: string | null
+          duration?: number | null
+          ebook_file?: Json | null
+          explicit?: boolean | null
+          genres?: Json | null
+          id: string
+          isbn?: string | null
+          language?: string | null
+          narrators?: Json | null
+          published_date?: string | null
+          published_year?: string | null
+          publisher?: string | null
+          subtitle?: string | null
+          tags?: Json | null
+          title?: string | null
+          updated_at?: string
+        }
+        Update: {
+          abridged?: boolean | null
+          asin?: string | null
+          audio_files?: Json | null
+          chapters?: Json | null
+          cover_path?: string | null
+          created_at?: string
+          description?: string | null
+          duration?: number | null
+          ebook_file?: Json | null
+          explicit?: boolean | null
+          genres?: Json | null
+          id?: string
+          isbn?: string | null
+          language?: string | null
+          narrators?: Json | null
+          published_date?: string | null
+          published_year?: string | null
+          publisher?: string | null
+          subtitle?: string | null
+          tags?: Json | null
+          title?: string | null
+          updated_at?: string
+        }
+        Relationships: []
+      }
+      legacy_users: {
+        Row: {
+          created_at: string
+          id: string
+          is_active: boolean | null
+          permissions: Json | null
+          updated_at: string
+          user_type: string | null
+          username: string | null
+        }
+        Insert: {
+          created_at?: string
+          id: string
+          is_active?: boolean | null
+          permissions?: Json | null
+          updated_at?: string
+          user_type?: string | null
+          username?: string | null
+        }
+        Update: {
+          created_at?: string
+          id?: string
+          is_active?: boolean | null
+          permissions?: Json | null
+          updated_at?: string
+          user_type?: string | null
+          username?: string | null
+        }
+        Relationships: []
+      }
+      libraries: {
+        Row: {
+          created_at: string
+          display_order: number | null
+          extra_data: Json | null
+          icon: string | null
+          id: string
+          media_type: string | null
+          name: string
+          provider: string | null
+          settings: Json | null
+          updated_at: string
+        }
+        Insert: {
+          created_at?: string
+          display_order?: number | null
+          extra_data?: Json | null
+          icon?: string | null
+          id: string
+          media_type?: string | null
+          name: string
+          provider?: string | null
+          settings?: Json | null
+          updated_at?: string
+        }
+        Update: {
+          created_at?: string
+          display_order?: number | null
+          extra_data?: Json | null
+          icon?: string | null
+          id?: string
+          media_type?: string | null
+          name?: string
+          provider?: string | null
+          settings?: Json | null
+          updated_at?: string
+        }
+        Relationships: []
+      }
+      library_items: {
+        Row: {
+          author_names_first_last: string | null
+          author_names_last_first: string | null
+          birthtime: string | null
+          created_at: string
+          ctime: string | null
+          extra_data: Json | null
+          id: string
+          ino: string | null
+          is_file: boolean | null
+          is_invalid: boolean | null
+          is_missing: boolean | null
+          last_scan: string | null
+          last_scan_version: string | null
+          library_files: Json | null
+          library_id: string | null
+          media_id: string | null
+          media_type: string | null
+          mtime: string | null
+          path: string | null
+          rel_path: string | null
+          size: number | null
+          title: string | null
+          title_ignore_prefix: string | null
+          updated_at: string
+        }
+        Insert: {
+          author_names_first_last?: string | null
+          author_names_last_first?: string | null
+          birthtime?: string | null
+          created_at?: string
+          ctime?: string | null
+          extra_data?: Json | null
+          id: string
+          ino?: string | null
+          is_file?: boolean | null
+          is_invalid?: boolean | null
+          is_missing?: boolean | null
+          last_scan?: string | null
+          last_scan_version?: string | null
+          library_files?: Json | null
+          library_id?: string | null
+          media_id?: string | null
+          media_type?: string | null
+          mtime?: string | null
+          path?: string | null
+          rel_path?: string | null
+          size?: number | null
+          title?: string | null
+          title_ignore_prefix?: string | null
+          updated_at?: string
+        }
+        Update: {
+          author_names_first_last?: string | null
+          author_names_last_first?: string | null
+          birthtime?: string | null
+          created_at?: string
+          ctime?: string | null
+          extra_data?: Json | null
+          id?: string
+          ino?: string | null
+          is_file?: boolean | null
+          is_invalid?: boolean | null
+          is_missing?: boolean | null
+          last_scan?: string | null
+          last_scan_version?: string | null
+          library_files?: Json | null
+          library_id?: string | null
+          media_id?: string | null
+          media_type?: string | null
+          mtime?: string | null
+          path?: string | null
+          rel_path?: string | null
+          size?: number | null
+          title?: string | null
+          title_ignore_prefix?: string | null
+          updated_at?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "library_items_library_id_fkey"
+            columns: ["library_id"]
+            isOneToOne: false
+            referencedRelation: "libraries"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "library_items_media_id_fkey"
+            columns: ["media_id"]
+            isOneToOne: false
+            referencedRelation: "books"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
       media_progress: {
         Row: {
           current_time_pos: number | null
@@ -50,6 +409,86 @@ export type Database = {
         }
         Relationships: []
       }
+      playback_sessions: {
+        Row: {
+          cover_path: string | null
+          created_at: string
+          current_time_pos: number | null
+          day_of_week: string | null
+          display_author: string | null
+          display_title: string | null
+          duration: number | null
+          extra_data: Json | null
+          id: string
+          library_id: string | null
+          media_item_id: string | null
+          media_item_type: string | null
+          media_metadata: Json | null
+          media_player: string | null
+          play_method: number | null
+          server_version: string | null
+          session_date: string | null
+          start_time_pos: number | null
+          time_listening: number | null
+          updated_at: string
+          user_id: string | null
+        }
+        Insert: {
+          cover_path?: string | null
+          created_at?: string
+          current_time_pos?: number | null
+          day_of_week?: string | null
+          display_author?: string | null
+          display_title?: string | null
+          duration?: number | null
+          extra_data?: Json | null
+          id: string
+          library_id?: string | null
+          media_item_id?: string | null
+          media_item_type?: string | null
+          media_metadata?: Json | null
+          media_player?: string | null
+          play_method?: number | null
+          server_version?: string | null
+          session_date?: string | null
+          start_time_pos?: number | null
+          time_listening?: number | null
+          updated_at?: string
+          user_id?: string | null
+        }
+        Update: {
+          cover_path?: string | null
+          created_at?: string
+          current_time_pos?: number | null
+          day_of_week?: string | null
+          display_author?: string | null
+          display_title?: string | null
+          duration?: number | null
+          extra_data?: Json | null
+          id?: string
+          library_id?: string | null
+          media_item_id?: string | null
+          media_item_type?: string | null
+          media_metadata?: Json | null
+          media_player?: string | null
+          play_method?: number | null
+          server_version?: string | null
+          session_date?: string | null
+          start_time_pos?: number | null
+          time_listening?: number | null
+          updated_at?: string
+          user_id?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "playback_sessions_library_id_fkey"
+            columns: ["library_id"]
+            isOneToOne: false
+            referencedRelation: "libraries"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
       profiles: {
         Row: {
           created_at: string | null
@@ -71,6 +510,65 @@ export type Database = {
           updated_at?: string | null
           user_type?: string | null
           username?: string | null
+        }
+        Relationships: []
+      }
+      series: {
+        Row: {
+          created_at: string
+          description: string | null
+          id: string
+          library_id: string | null
+          name: string | null
+          name_ignore_prefix: string | null
+          updated_at: string
+        }
+        Insert: {
+          created_at?: string
+          description?: string | null
+          id: string
+          library_id?: string | null
+          name?: string | null
+          name_ignore_prefix?: string | null
+          updated_at?: string
+        }
+        Update: {
+          created_at?: string
+          description?: string | null
+          id?: string
+          library_id?: string | null
+          name?: string | null
+          name_ignore_prefix?: string | null
+          updated_at?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "series_library_id_fkey"
+            columns: ["library_id"]
+            isOneToOne: false
+            referencedRelation: "libraries"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      server_settings: {
+        Row: {
+          created_at: string
+          key: string
+          updated_at: string
+          value: Json | null
+        }
+        Insert: {
+          created_at?: string
+          key: string
+          updated_at?: string
+          value?: Json | null
+        }
+        Update: {
+          created_at?: string
+          key?: string
+          updated_at?: string
+          value?: Json | null
         }
         Relationships: []
       }
