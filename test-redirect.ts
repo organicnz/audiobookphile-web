64b65b14b748a3489dc8ee837357092f78acd310
organicnz <tamerlanium@gmail.com>
2026-05-30 23:41:23 -0700

    Cleanup test file

--- test-redirect.ts
+++
@@ -1,9 +0,0 @@
-import { redirect } from 'next/navigation'
-
-try {
-  redirect('/foo')
-} catch (error: any) {
-  console.log('Message:', error.message)
-  console.log('Digest:', error.digest)
-  console.log('Includes NEXT_REDIRECT?', error.digest?.includes('NEXT_REDIRECT'))
-}
