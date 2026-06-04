8a48784345d801cd403337f28caea59c4472b330
organicnz <tamerlanium@gmail.com>
2026-06-03 20:36:07 -0700

    test: add Cypress E2E framework

--- cypress.config.ts
+++ cypress.config.ts
@@ -26,5 +26,13 @@ export default defineConfig({
         return launchOptions
       })
     }
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: false
   }
 })
